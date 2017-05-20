fdb243c3703dddd83c09b0d45a8ba3459701490c
Olaf Greck <olaf@greck.eu>
2017-05-20 15:52:38 +0200

    adjusted offset between Probe and Extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -639,7 +639,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 10  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 60  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.5   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.3 // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
